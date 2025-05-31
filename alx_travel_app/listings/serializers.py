3459d0a7c5eb1132e817dd259d4ecf54827208c4
Gatlat-cs2023 <gatlatboth@gmail.com>
2025-05-31 16:56:36 +0300

    New Commit

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -6,6 +6,7 @@ class ListingSerializer(serializers.ModelSerializer):
         model = Listing
         fields = '__all__'
 
+
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
